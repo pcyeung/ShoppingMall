ef3f64fa5ff1ef055c0c56a311ee502db56ebff7
alexccy <alexcheung_cy@hotmail.com>
2014-02-14 16:33:39 +0800

    ScanQR header is added

---
+++ ShoppingMall/ScanQRViewController.h
@@ -0,0 +1,15 @@
+//
+//  ScanQRViewController.h
+//  ShoppingMall
+//
+//  Created by Alex Cheung on 14/2/14.
+//  Copyright (c) 2014 Macie. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ScanQRViewController : BaseViewController<UITableViewDataSource,UITableViewDelegate>{
+    IBOutlet UIButton *QRCodeBtn;
+}
+
+@end
