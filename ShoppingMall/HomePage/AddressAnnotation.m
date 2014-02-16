c917a7f080de228a1cccce49a7449a317da46b60
pcyeung <pcyeung1990@gmail.com>
2014-02-16 17:43:59 +0800

    Drop Pin to Map prototype

---
+++ ShoppingMall/HomePage/AddressAnnotation.m
@@ -0,0 +1,19 @@
+#import "AddressAnnotation.h"
+
+@implementation AddressAnnotation
+@synthesize coordinate;
+
+- (NSString *)subtitle{
+	return nil;
+}
+
+- (NSString *)title{
+	return nil;
+}
+
+-(id)initWithCoordinate:(CLLocationCoordinate2D) c{
+	coordinate=c;
+	return self;
+}
+
+@end
