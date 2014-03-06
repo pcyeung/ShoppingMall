9a4679177dce8589576e4206c35b84c08d61695c
pcyeung <pcyeung1990@gmail.com>
2014-03-07 01:56:53 +0800

    Dynamic Show Mall Page Logos

--- ShoppingMall/MallPage/MallDetail/ShoppingMallIndexDetailViewController.m
+++ ShoppingMall/MallPage/MallDetail/ShoppingMallIndexDetailViewController.m
@@ -46,6 +46,31 @@
     
     mallName.text = [_mall mallName];
     mallAddress.text = [_mall mallAddress];
+    if([mallName.text isEqualToString:@"庄胜崇光"])
+    {
+        UIImage *mallImage = [UIImage imageNamed:@"mall2.jpg"];
+        UIImage *btn1 = [UIImage imageNamed:@"logo6.jpg"];
+        UIImage *btn2 = [UIImage imageNamed:@"logo7.jpg"];
+        UIImage *btn3 = [UIImage imageNamed:@"logo8.jpg"];
+        UIImage *btn4 = [UIImage imageNamed:@"logo9.jpg"];
+        
+        [shoppingMallBannerBtn setImage:mallImage forState:UIControlStateNormal];
+        [adButton1 setImage:btn1];
+        [adButton1 setContentMode:UIViewContentModeScaleAspectFit];
+        [adButton2 setImage:btn2];
+        [adButton2 setContentMode:UIViewContentModeScaleAspectFit];
+
+        [adButton3 setImage:btn3];
+        [adButton3 setContentMode:UIViewContentModeScaleAspectFit];
+
+        [adButton5 setImage:btn4];
+        [adButton5 setContentMode:UIViewContentModeScaleAspectFit];
+
+
+
+
+
+    }
     [self setUpAdButton:adButton1];
     [self setUpAdButton:adButton2];
     [self setUpAdButton:adButton3];
